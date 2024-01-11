9b06629fe1104083edc646dbf1cabd93f30b17c7
yuwenbo <yyuweb@outlook.com>
2024-01-11 15:20:10 +0800

    fix:

--- spring-demo/src/main/java/org/springframework/demo/basic/beans/DemoMain.java
+++ spring-demo/src/main/java/org/springframework/demo/basic/beans/DemoMain.java
@@ -11,7 +11,6 @@ public class DemoMain {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 						"xml/Demo.xml");
-		context.setAllowCircularReferences(false);
 		DependencyDemoA dependencyDemoA = (DependencyDemoA) context.getBean("dependencyDemoA");
 		dependencyDemoA.sayHello();
 	}
